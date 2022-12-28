96a0928b66bbd1c98e2912d82dbc10da5f42d601
A. Batten <drew.k.batten@gmail.com>
2022-12-28 10:15:28 -0500

    comm msn. points initialize to surface of earth

--- Assets/Missions/CommMission.cs
+++ Assets/Missions/CommMission.cs
@@ -20,21 +20,22 @@ public class CommMission : MonoBehaviour {
 		float lon = Random.Range(-179.0f, 180.0f);
 		float lat = Random.Range(-89.0f, 89.0f);
 
-		point1.transform.position = new Vector3(6371 / sf, 0, 0);
-		point1.transform.Rotate(Vector3.up, lon);
+		// point the X-axis in a random direction (from above), then move forward to the surface of the Earth
+		point1.transform.Rotate(new Vector3(0, 1, 0), lon);
 		point1.transform.Rotate(new Vector3(0, 0, 1), lat, Space.Self);
+		point1.transform.Translate((6371f / sf), 0, 0, Space.Self);
 
 
 		// this section creates an offset for the 2nd point, in the area of the first, but not too close
 		// "overflows" are fine since we're dealing with spheres and degrees
 		float offset = 0f;
-		// create a somewhat Gaussian distribution for the offset. Maximum 90 degree seperation, but that's pretty unlikely
+		// create a somewhat Gaussian distribution for the offset. Maximum 90 degree separation, but that's pretty unlikely
 		// Also a chance to have the offset be negative
 		for (int i = 0; i < 5; i++) {
 			offset += Random.Range(-2f, 18f);
 		}
 		// 50/50 chance to flip the sign.
-		// For visualizing the probability distriibution, this creates a curve made from 2 gaussian-ish distributions with some overlap.
+		// For visualizing the probability distribution, this creates a curve made from 2 Gaussian-ish distributions with some overlap.
 		// the center of the overlap is the original point
 		if (Random.Range(0, 2) == 1) {
 			offset *= -1;
@@ -51,9 +52,9 @@ public class CommMission : MonoBehaviour {
 		}
 		lat += offset;
 
-		point2.transform.position = new Vector3(6371 / sf, 0, 0);
-		point2.transform.Rotate(Vector3.up, lon);
+		point2.transform.Rotate(new Vector3(0, 1, 0), lon);
 		point2.transform.Rotate(new Vector3(0, 0, 1), lat, Space.Self);
+		point2.transform.Translate((6371f / sf), 0, 0, Space.Self);
 
 		reward = Random.Range(minReward, maxReward+1);
 	}
