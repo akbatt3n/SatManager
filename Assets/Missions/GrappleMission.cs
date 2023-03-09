9cb1bcc85aba2b8db95468d50605469d344f8278
A. Batten <20829790+akbatt3n@users.noreply.github.com>
2023-03-09 11:02:36 -0500

    designed/started grapple payload

--- Assets/Missions/GrappleMission.cs
+++ Assets/Missions/GrappleMission.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class GrappleMission : MonoBehaviour {
     
     public Satellite target;
+    public GameObject targetObj;
     public float minAlt;
     public float maxAlt;
-    
+
     int sf;
 
     // reward in thousands of dollars. needs to be balanced
@@ -22,12 +23,24 @@ public class GrappleMission : MonoBehaviour {
 
     public bool selected = false;
 
+    public void Start() {
+        // create target satellite
+        // link to mission
+    }
+
     public bool checkComplete() {
         if (target.altitude > minAlt && target.altitude < maxAlt) return true;
         else return false;
     }
 
     public void toggleHighlight() {
-
+        if (selected) {
+            // show target sat orbit/position
+            // show orbital parameters in seperate window
+        }
+        else {
+            // hide target sat
+            // hide orbital parameter window
+        }
     }
 }
--- Assets/Satellites/grapplePayload.cs
+++ Assets/Satellites/grapplePayload.cs
@@ -3,7 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class grapplePayload : MonoBehaviour {
-    
 
+    public GameObject grappleMissions;
+    public float maxDistanceMm = 0.001f; // Mm = 1000Km
+    public GameObject heldObject;
     
+    public void toggleGrapple() {
+        if (heldObject == null) {
+            foreach (Transform mission in grappleMissions.transform) {
+                // "mission" var is the transform, need to get the parent game object, then the script, 
+                // then the var that references the target satellite object, then it's position
+                GrappleMission temp = mission.parent.GetComponent<GrappleMission>();
+                if (Vector3.Distance(transform.position, temp.targetObj.transform.position) > maxDistanceMm) {
+                    break;
+                }
+                heldObject = temp.gameObject;
+                enabled = true;
+            }
+        }
+        else {
+            heldObject = null;
+            enabled = false;
+        }
+    }
+
+    void Update() {
+
+    }
 }
